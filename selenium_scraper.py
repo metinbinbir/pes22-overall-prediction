31779f73a99631ceeafd25ceb0ccfba900e77f8d
metinbinbir <binbir.metin@hotmail.com>
2022-05-16 02:50:06 +0300

    data scraping and preprocessing done

---
+++ selenium_scraper.py
@@ -0,0 +1,92 @@
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
+
+def continue_after_req_limit(start_param):
+    """
+    :param start_param: request limitine takildigim icin bu parametre ile kaldigim yerden veriyi cekip kaydetmeye devam ettim
+    :return:
+    """
+
+    # verileri cekmek icin firefox kullanilmistir, geckodriver dosyasi bu dosya ile ayni pathte yer almalidir
+    f_options = Options()
+    f_options.add_argument('--no-sandbox')
+    f_options.add_argument('--disable-dev-shm-usage')
+    driver_path = "geckodriver.exe"
+    w = open("player_and_stats.txt","a",encoding="utf-8")
+    browser = webdriver.Firefox(executable_path=driver_path,options=f_options)
+    browser.implicitly_wait(4)
+
+    j = 0
+    # toplamda sitede 558 sayfa yer almaktadir
+    for i in range(start_param,558):
+        print(i)
+        browser.get("https://pesdb.net/pes2022/" + "?page=" + str(i))
+        if j == 0:
+            # attributelarin secilmesi icin sitenin sol altinda yer alan settings panelinin otomatik olarak acilmasi
+            settings_button_xpath = '// *[ @ id = "filters"] / div[2] / button'
+            settings_button = browser.find_element_by_xpath(settings_button_xpath)
+            browser.execute_script("arguments[0].click();", settings_button)
+
+            # attributelarin checkbox iceren listedeki xpath idlerini ogeyi denetle secenegi ile bulup degiskenlere atadim
+            # ulke ve kulup ismi hem preprocessi bozuyor hem de cok farkli sayida sample'a sahip, bu yuzden bunlar da otomatik olarak unticked olacak
+            country_xpath = '//*[@id="col_club_team"]'
+            nationality_xpath = '//*[@id="col_nationality"]'
+
+
+            stronger_foot_xpath = '//*[@id="col_foot"]'
+            ball_control_xpath = '//*[@id="col_ball_control"]'
+            dribbling_xpath = '//*[@id="col_dribbling"]'
+            low_pass_xpath = '//*[@id="col_low_pass"]'
+            lofted_pass_xpath = '//*[@id="col_lofted_pass"]'
+            finishing_path = '//*[@id="col_finishing"]'
+            speed_xpath = '//*[@id="col_speed"]'
+            kicking_power_xpath = '//*[@id="col_kicking_power"]'
+            stamina_xpath = '//*[@id="col_stamina"]'
+            tackling_xpath = '//*[@id="col_tackling"]'
+            aggression_xpath = '//*[@id="col_aggression"]'
+            def_eng_xpath = '//*[@id="col_defensive_engagement"]'
+            wf_acc_xpath = '//*[@id="col_weak_foot_accuracy"]'
+
+            # xpathler listeye atilir
+            checkbox_xpath_list = [country_xpath, nationality_xpath,stronger_foot_xpath,ball_control_xpath,dribbling_xpath,low_pass_xpath,lofted_pass_xpath,def_eng_xpath,
+                                   finishing_path,speed_xpath,kicking_power_xpath,stamina_xpath,tackling_xpath,aggression_xpath,wf_acc_xpath]
+
+            # bu liste icinde iterate edilerek tabloya yansitilacak attributelar otomatik olarak isaretlenir
+            for cb_xpath in checkbox_xpath_list:
+                check_box = browser.find_element_by_xpath(cb_xpath)
+                browser.execute_script("arguments[0].click();", check_box)
+
+            # secili attributelar yansitmak icin apply butonuna otomatik olarak basilir
+            apply_button_xpath = '//*[@id="popup0"]/div[3]/button'
+            apply_button = browser.find_element_by_xpath(apply_button_xpath)
+            browser.execute_script("arguments[0].click();", apply_button)
+
+            # scriptin her calistirilmasinda ilk sayfadan sonra tekrardan secili attributelari isaretlemek gerekmez
+            # j variable'i ile bu durum saglanir
+            j += 1
+            time.sleep(3)
+
+            # yeni attributelarla olusmus tablo ve veriler selenium icindeki function ile string olarak alinir ve txt dosyasina yazilir
+            table = browser.find_element_by_xpath('//*[@id="content"]/div[2]/table').text
+            w.write(table + "\n")
+            print(table)
+
+        # yukarida da aciklandigi uzere script calistirildiktan sonraki ilk sayfa disinda gelen tum tablolar direkt txt dosyasina kaydedilir
+        else:
+            table = browser.find_element_by_xpath('//*[@id="content"]/div[2]/table').text
+            w.write(table+"\n")
+
+"""
+100 kusur iterasyondan sonra sitenin request limitine takildigim icin kacinci iterasyonda hata aldigimi anlamak uzere i variable'ini yazdirdim
+txt dosyasini da a flagi ile acmistim, request limiti yaklasik 1.5-2 dk icinde sifirlanmakta, toplamda 10-15 dkda her seferinde nereden kaldigimi belirten 
+i parametresi ile functionu cagirdim ve tum datayi alt alta ekledim
+"""
+# continue_after_req_limit(1)
+# continue_after_req_limit(118)
+# continue_after_req_limit(236)
+# continue_after_req_limit(353)
+# continue_after_req_limit(470)
---
+++ txt_to_pd.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+def is_integer(string):
+    if string[0] == ('-', '+'):
+        return string[1:].isdigit()
+
+    else:
+        return string.isdigit()
+
+def txt_to_dataframe(filename):
+    file_content_list = open(filename,"r",encoding="utf-8").read().splitlines()
+    only_data_list = [x for x in file_content_list if "Position Player Name" not in x]
+    columns = ["Position", "Height", "Weight", "Age", "Preferred Foot", "Avg Ball", "Avg Pass", "Finishing", "Speed",
+               "Kicking Power", "Stamina", "Avg Def", "Weak Foot", "Overall"]
+    df = pd.DataFrame(columns=columns)
+    for row in only_data_list:
+        # print(row)
+        splitted_attributes = row.split(" ")
+        position = splitted_attributes[0]
+
+        for i in range(len(splitted_attributes)):
+            if is_integer(splitted_attributes[i]):
+                curr_index = i
+                break
+        row_values = []
+        row_values.append(position)
+        height = int(splitted_attributes[curr_index])
+        row_values.append(height)
+        try:
+            weight = int(splitted_attributes[curr_index+1])
+        except ValueError:
+            continue
+        row_values.append(weight)
+
+        age = int(splitted_attributes[curr_index+2])
+        row_values.append(age)
+
+        preferred_foot = splitted_attributes[curr_index +3]
+        row_values.append(preferred_foot)
+
+        ball_control = int(splitted_attributes[curr_index+5])
+        dribbling = int(splitted_attributes[curr_index+6])
+        avg_ball = (ball_control + dribbling) / 2
+        row_values.append(avg_ball)
+
+        low_pass = int(splitted_attributes[curr_index+7])
+        lofted_pass = int(splitted_attributes[curr_index+8])
+        pass_avg = (low_pass + lofted_pass) / 2
+        row_values.append(pass_avg)
+
+        finishing = int(splitted_attributes[curr_index+9])
+        row_values.append(finishing)
+
+        speed = int(splitted_attributes[curr_index+10])
+        row_values.append(speed)
+
+        kicking_power = int(splitted_attributes[curr_index+11])
+        row_values.append(kicking_power)
+
+        stamina = int(splitted_attributes[curr_index+12])
+        row_values.append(stamina)
+
+        tackling = int(splitted_attributes[curr_index+13])
+        aggression = int(splitted_attributes[curr_index+14])
+        def_engagement = int(splitted_attributes[curr_index+15])
+        def_avg = (tackling+aggression+def_engagement)/3
+        row_values.append(def_avg)
+
+        weak_foot = splitted_attributes[curr_index+16]
+        row_values.append(weak_foot)
+
+        overall = int(splitted_attributes[-1])
+        row_values.append(overall)
+
+        df = df.append(pd.DataFrame([row_values], columns=columns))
+
+
+    df.to_csv("data.csv",encoding="utf-8",index=False)
+
+txt_to_dataframe("player_and_stats.txt")
